a76ff94e68144110d9d93aa7b351b5be7243d656
razvancir96 <razvan.cirlugea@gmail.com>
2021-10-18 19:29:49 +0300

    create footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+
+function Footer() {
+  return (
+    <footer className="bg-dark">
+      <Container>
+        <p className="text-light text-center m-0 py-3">
+          IT School România © 2021. Toate drepturile rezervate.
+        </p>
+      </Container>
+    </footer>
+  );
+}
+
+export default Footer;
