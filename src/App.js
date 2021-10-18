aac4dd3423088f2ff19e12ec21dcfcc034025825
razvancir96 <razvan.cirlugea@gmail.com>
2021-10-18 19:30:16 +0300

    instantiate created components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Page404 from "./pages/Page404";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <Footer />
+      <Page404 />
+    </div>
+  );
 }
 
 export default App;
