e91d4492436dc8dc2c03ec6f1b7f2d0ce2a821a2
razvancir96 <razvan.cirlugea@gmail.com>
2021-10-19 12:46:57 +0300

    add api endpoints

---
+++ src/api/endpoints.js
@@ -0,0 +1,17 @@
+const API_KEY = "cce4027e-faf6-4eb5-ba9b-491c9b8f3571";
+
+export function getNewsCategoriesEndpoint(
+  category,
+  pageNumber = 1,
+  pageSize = 20
+) {
+  const queryParams = `?api-key=${API_KEY}&section=${category}&show-fields=all&page-size=${pageSize}&page=${pageNumber}`;
+
+  return `https://content.guardianapis.com/search${queryParams}`;
+}
+
+export function getNewsDetailsEndpoint(newsId) {
+  const queryParams = `?api-key=${API_KEY}&show-fields=all`;
+
+  return `https://content.guardianapis.com/${newsId}${queryParams}`;
+}
