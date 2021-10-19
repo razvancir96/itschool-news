138da82141cfeb1b8566f2aab242ac0a3d19537b
razvancir96 <razvan.cirlugea@gmail.com>
2021-10-19 12:48:10 +0300

    fetch data and log it

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 import Layout from "../components/Layout";
 import Container from "react-bootstrap/Container";
+import { useFetch } from "../utils/hooks/useFetch";
+import { getNewsCategoriesEndpoint } from "../api/endpoints";
 
 function Home() {
+  const technologyNewsEndpoint = getNewsCategoriesEndpoint("technology", 1, 8);
+  const footballNewsEndpoint = getNewsCategoriesEndpoint("football", 1, 8);
+  const technologyData = useFetch(technologyNewsEndpoint);
+  const footballData = useFetch(footballNewsEndpoint);
+
+  console.log(technologyData, footballData);
+
   return (
     <Layout>
       <section className="tech">
--- src/pages/NewsCategory.jsx
+++ src/pages/NewsCategory.jsx
@@ -2,9 +2,15 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import Layout from "../components/Layout";
 import Container from "react-bootstrap/Container";
+import { useFetch } from "../utils/hooks/useFetch";
+import { getNewsCategoriesEndpoint } from "../api/endpoints";
 
 function NewsCategory() {
   const { categoryId } = useParams();
+  const newsCategoryEndpoint = getNewsCategoriesEndpoint(categoryId);
+  const news = useFetch(newsCategoryEndpoint);
+
+  console.log(news);
 
   return (
     <Layout>
