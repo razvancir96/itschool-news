caeb1cc3f146b6d736a0750fd2a63da050329c04
razvancir96 <razvan.cirlugea@gmail.com>
2021-10-20 13:30:02 +0300

    display content in `NewsCategory` page

--- src/pages/NewsCategory.jsx
+++ src/pages/NewsCategory.jsx
@@ -4,19 +4,32 @@ import Layout from "../components/Layout";
 import Container from "react-bootstrap/Container";
 import { useFetch } from "../utils/hooks/useFetch";
 import { getNewsCategoriesEndpoint } from "../api/endpoints";
+import { getNewsList } from "../api/adaptors";
+import NewsCardList from "../components/NewsCardList";
 
 function NewsCategory() {
   const { categoryId } = useParams();
   const newsCategoryEndpoint = getNewsCategoriesEndpoint(categoryId);
   const news = useFetch(newsCategoryEndpoint);
+  const adaptedNewsList = getNewsList(news);
 
-  console.log(news);
+  let title = "";
+  switch (categoryId) {
+    case "technology":
+      title = "Tech";
+      break;
+    case "football":
+      title = "Fotbal";
+      break;
+    default:
+      break;
+  }
 
   return (
     <Layout>
-      <Container>
-        <h1>Numele categoriei</h1>
-        <p>Parametrul venit din rută: {categoryId}</p>
+      <Container className="my-5">
+        <h1 className="mb-5 pt-3">{title}</h1>
+        <NewsCardList newsList={adaptedNewsList} />
       </Container>
     </Layout>
   );
