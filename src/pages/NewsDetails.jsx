a892a1c039b0ea9b57fb9e829a78eb1d0bd3c8ee
razvancir96 <razvan.cirlugea@gmail.com>
2021-10-20 13:32:05 +0300

    display data for `NewsDetails` page

--- src/pages/NewsDetails.jsx
+++ src/pages/NewsDetails.jsx
@@ -2,15 +2,46 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import Layout from "../components/Layout";
 import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { useFetch } from "../utils/hooks/useFetch";
+import { getNewsDetailsEndpoint } from "../api/endpoints";
+import { getNewsDetails } from "../api/adaptors";
+import Button from "react-bootstrap/Button";
+import styles from "./NewsDetails.module.css";
+import { getFormattedDate } from "../utils/date";
 
 function NewsDetails() {
   const { newsId } = useParams();
+  const newsDetailsEndpoint = getNewsDetailsEndpoint(newsId);
+  const newsDetails = useFetch(newsDetailsEndpoint);
+  const adaptedNewsDetails = getNewsDetails(newsDetails);
+
+  const { title, description, image, date, author, content } =
+    adaptedNewsDetails;
+  const formattedDate = getFormattedDate(date);
 
   return (
     <Layout>
-      <Container>
-        <h1>Titlul știrii</h1>
-        <p>Parametrul venit din rută: {newsId}</p>
+      <Container className={`${styles.newsDetails} my-5`}>
+        <Row className="d-flex justify-content-center">
+          <Col xs={12} lg={8}>
+            <h1 className="pt-3 mb-5">{title}</h1>
+            <p className="fw-bold">{description}</p>
+            <div
+              dangerouslySetInnerHTML={{ __html: image }}
+              className="mb-4"
+            ></div>
+            <div className="d-flex justify-content-between align-items-center mb-4">
+              <div className="fw-bold">
+                <p>{author}</p>
+                <p className="mb-0">{formattedDate}</p>
+              </div>
+              <Button>Adaugă la favorite</Button>
+            </div>
+            <div dangerouslySetInnerHTML={{ __html: content }}></div>
+          </Col>
+        </Row>
       </Container>
     </Layout>
   );
