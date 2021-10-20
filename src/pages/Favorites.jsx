ffe15708374fc71a5f2e4ea236564b3a188e9676
razvancir96 <razvan.cirlugea@gmail.com>
2021-10-20 16:58:52 +0300

    display favorite news

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,12 +1,21 @@
-import React from "react";
+import { useContext } from "react";
 import Layout from "../components/Layout";
 import Container from "react-bootstrap/Container";
+import { FavoritesContext } from "../store/Favorites/context";
+import NewsCardList from "../components/NewsCardList";
 
 function Favorites() {
+  const { favoritesState } = useContext(FavoritesContext);
+  const { products } = favoritesState;
   return (
     <Layout>
-      <Container>
-        <p>Momentan nu ai nicio știre favorită.</p>
+      <Container className="my-5">
+        <h1 className="mb-5 pt-3">Știrile tale favorite</h1>
+        {products.length === 0 ? (
+          <p>Momentan nu ai nicio știre favorită.</p>
+        ) : (
+          <NewsCardList newsList={products} />
+        )}
       </Container>
     </Layout>
   );
