ca331ddde16a33b93d041ef8b46dfe2ab39cb25a
razvancir96 <razvan.cirlugea@gmail.com>
2021-10-20 13:29:14 +0300

    display news lists in home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,30 +3,64 @@ import Layout from "../components/Layout";
 import Container from "react-bootstrap/Container";
 import { useFetch } from "../utils/hooks/useFetch";
 import { getNewsCategoriesEndpoint } from "../api/endpoints";
+import NewsCardList from "../components/NewsCardList";
+import { getNewsList } from "../api/adaptors";
+import { Link } from "react-router-dom";
 
 function Home() {
-  const technologyNewsEndpoint = getNewsCategoriesEndpoint("technology", 1, 8);
-  const footballNewsEndpoint = getNewsCategoriesEndpoint("football", 1, 8);
-  const technologyData = useFetch(technologyNewsEndpoint);
-  const footballData = useFetch(footballNewsEndpoint);
+  const technologyNewsEndpoint = getNewsCategoriesEndpoint("technology", 1, 6);
+  const footballNewsEndpoint = getNewsCategoriesEndpoint("football", 1, 6);
+  let technologyData = useFetch(technologyNewsEndpoint);
+  let footballData = useFetch(footballNewsEndpoint);
 
-  console.log(technologyData, footballData);
+  const adaptedTechnologyData = getNewsList(technologyData);
+  const adaptedFootballData = getNewsList(footballData);
 
   return (
     <Layout>
-      <section className="tech">
+      <section className="tech my-5">
         <Container>
-          <h1>Tech</h1>
+          <h1 className="mb-5 pt-3">Tech</h1>
+          <NewsCardList newsList={adaptedTechnologyData} />
+          <p>
+            Vezi toate știrile legate de tehnologie în secțiunea{" "}
+            <Link to="/category/technology" className="text-secondary">
+              Tech
+            </Link>
+            .
+          </p>
         </Container>
       </section>
-      <section className="football">
+      <section className="football my-5">
         <Container>
-          <h1>Fotbal</h1>
+          <h1 className="mb-5 pt-3">Fotbal</h1>
+          <NewsCardList newsList={adaptedFootballData} />
+          <p>
+            Vezi toate știrile legate de tehnologie în secțiunea{" "}
+            <Link to="/category/football" className="text-secondary">
+              Fotbal
+            </Link>
+            .
+          </p>
         </Container>
       </section>
-      <section className="favorites">
+      <section className="favorites my-5">
         <Container>
-          <h1>Favorite</h1>
+          <h1 className="mb-5 pt-3">Favorite</h1>
+          <p>
+            Vrei să îți salvezi știrile favorite pentru a le reciti mai încolo?
+          </p>
+          <p>
+            În cadrul fiecărei știri găsești un buton prin care poți adăuga
+            știrea la favorite.
+          </p>
+          <p className="pb-3">
+            Vizitează secțiunea{" "}
+            <Link to="/favorites" className="text-secondary">
+              Favorite
+            </Link>{" "}
+            pentru a vedea știrile adăugate.
+          </p>
         </Container>
       </section>
     </Layout>
