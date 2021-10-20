cebd099e199d873e21b6146f20e00eb82261973d
razvancir96 <razvan.cirlugea@gmail.com>
2021-10-20 13:27:17 +0300

    add `NewsCardList` component

---
+++ src/components/NewsCardList.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import NewsCard from "./NewsCard";
+
+function NewsCardList(props) {
+  const { newsList } = props;
+
+  return (
+    <Container>
+      <Row>
+        {newsList.map((news) => {
+          return (
+            <Col xs={12} md={6} lg={4} className="mb-4" key={news.id}>
+              <NewsCard
+                newsId={news.id}
+                imgSrc={news.thumbnail}
+                title={news.title}
+                description={news.description}
+              />
+            </Col>
+          );
+        })}
+      </Row>
+    </Container>
+  );
+}
+
+export default NewsCardList;
