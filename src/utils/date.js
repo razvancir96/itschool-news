71e159d91b34cba90bb6c9dc49576fad612ebf15
razvancir96 <razvan.cirlugea@gmail.com>
2021-10-20 13:31:34 +0300

    add date utils functions

---
+++ src/utils/date.js
@@ -0,0 +1,15 @@
+export function getFormattedDate(dateString) {
+  const currentDate = new Date(dateString);
+  let month = currentDate.getMonth();
+  month = month + 1;
+  let day = currentDate.getDate();
+  let year = currentDate.getFullYear();
+  if (day < 10) {
+    day = "0" + day;
+  }
+  if (month < 10) {
+    month = "0" + month;
+  }
+
+  return `${day}/${month}/${year}`;
+}
