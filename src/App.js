68f8cd1d2e7f6fdff182b329b8fada762a6b45c4
razvancir96 <razvan.cirlugea@gmail.com>
2021-10-20 16:56:45 +0300

    initialize context and reducer

--- src/App.js
+++ src/App.js
@@ -4,27 +4,41 @@ import Home from "./pages/Home";
 import Favorites from "./pages/Favorites";
 import NewsCategory from "./pages/NewsCategory";
 import NewsDetails from "./pages/NewsDetails";
+import { useReducer } from "react";
+import { FavoritesContext } from "./store/Favorites/context";
+import { initialState, favoritesReducer } from "./store/Favorites/reducer";
 
 function App() {
+  const [favoritesState, favoritesDispatch] = useReducer(
+    favoritesReducer,
+    initialState
+  );
+  const favoritesContextValue = {
+    favoritesState,
+    favoritesDispatch,
+  };
+
   return (
     <div className="App">
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/favorites">
-          <Favorites />
-        </Route>
-        <Route path="/category/:categoryId">
-          <NewsCategory />
-        </Route>
-        <Route path="/news/:newsId*">
-          <NewsDetails />
-        </Route>
-        <Route path="*">
-          <Page404 />
-        </Route>
-      </Switch>
+      <FavoritesContext.Provider value={favoritesContextValue}>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/favorites">
+            <Favorites />
+          </Route>
+          <Route path="/category/:categoryId">
+            <NewsCategory />
+          </Route>
+          <Route path="/news/:newsId*">
+            <NewsDetails />
+          </Route>
+          <Route path="*">
+            <Page404 />
+          </Route>
+        </Switch>
+      </FavoritesContext.Provider>
     </div>
   );
 }
