988992067973d1cc8767d02e0c194d0944b5cad8
razvancir96 <razvan.cirlugea@gmail.com>
2021-10-20 21:42:30 +0300

    make sticky header work

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,7 +16,7 @@ function Header() {
   }
 
   return (
-    <header>
+    <header className={styles.header}>
       <nav className={`${styles.nav} bg-primary w-100`}>
         <Container className="d-flex justify-content-between align-items-center">
           <Link to="/" className="p-3">
